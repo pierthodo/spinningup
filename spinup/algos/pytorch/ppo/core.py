3390085ca66d1e4e22b09b373812ede75e23da5b
pierthodo <pierthodo@gmail.com>
2022-04-18 14:39:17 +0100

    Update core.py

--- spinup/algos/pytorch/ppo/core.py
+++ spinup/algos/pytorch/ppo/core.py
@@ -129,7 +129,11 @@ class MLPActorCritic(nn.Module):
             a = pi.sample()
             logp_a = self.pi._log_prob_from_distribution(pi, a)
             v = self.v(obs)
-        return a.numpy(), v.numpy(), logp_a.numpy()
+        if a.is_cuda:
+            return a.cpu().numpy(), v.cpu().numpy(), logp_a.cpu().numpy()
+        else:
+            return a.numpy(), v.numpy(), logp_a.numpy()
+
 
     def act(self, obs):
         return self.step(obs)[0]
